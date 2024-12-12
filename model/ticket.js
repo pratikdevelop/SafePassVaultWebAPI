4bc82b5306e84cc5e081a4681e3fe811826ff4fa
pratikdeveloper <pratikraut888952gmail.com>
2024-12-13 00:47:22 +0530

    working on crete API for report issue page for user

---
+++ model/ticket.js
@@ -0,0 +1,41 @@
+const ticketSchema = new mongoose.Schema({
+  ticketId: {
+    type: String,
+    unique: true,
+    default: () => `TICKET-${Date.now()}`,
+  },
+  name: String,
+  email: String,
+  category: String,
+  categorySubtype: String,
+  description: String,
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Medium",
+  },
+  severity: {
+    type: String,
+    enum: ["Minor", "Moderate", "Critical"],
+    default: "Moderate",
+  },
+  status: {
+    type: String,
+    default: "Open",
+    enum: ["Open", "In Progress", "Resolved", "Closed"],
+  },
+  assignee: String,
+  resolutionNotes: String,
+  attachments: [String],
+  userAgent: String,
+  relatedTickets: [{ type: mongoose.Schema.Types.ObjectId, ref: "Ticket" }],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+ticketSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+const Ticket = mongoose.model("Ticket", ticketSchema);
